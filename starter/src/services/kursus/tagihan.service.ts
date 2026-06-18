65b4c4c0e9955ece49d8927fe73e59faacaf02c6
Ideora Teknologi Nusantara <ideorateknologinusantara@gmail.com>
2026-06-18 09:17:05 +0700

    perbaiki redirect

--- starter/src/services/kursus/tagihan.service.ts
+++ starter/src/services/kursus/tagihan.service.ts
@@ -93,8 +93,7 @@ const TagihanService = {
             url: API_ENDPOINTS.KURSUS.TAGIHAN.CETAK(id),
             method: 'GET',
         })
-        const backendUrl = process.env.NEXT_PUBLIC_BACKEND_API_URL || 'http://localhost:4012'
-        window.open(`${backendUrl}${res.data.url}`, '_blank')
+        window.open(`/api/proxy${res.data.url}`, '_blank')
     },
 
     async addDetail(id: string, payload: IAddDetailTagihan): Promise<ApiResponse<ITagihan>> {
